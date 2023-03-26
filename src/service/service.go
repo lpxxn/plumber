7d6b9ba9139f513fd351d0b86d01981ec9c45d7d
lpxxn <mi_duo@live.com>
2023-03-26 22:37:32 +0800

    feat: service

---
+++ src/service/service.go
@@ -0,0 +1,34 @@
+package service
+
+import "net"
+
+type Service struct {
+	SrvAddr  string
+	listener net.Listener
+}
+
+func NewService(srvAddr string) *Service {
+	return &Service{
+		SrvAddr: srvAddr,
+	}
+}
+
+func (s *Service) Start() {
+	var err error
+	s.listener, err = net.Listen("tcp", s.SrvAddr)
+	if err != nil {
+		panic(err)
+	}
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go s.handleConnection(conn)
+	}
+}
+
+func (s *Service) handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+}
