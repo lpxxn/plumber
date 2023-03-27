212b208441c6e7da8909d4654549d296b82b1afc
Aiden <mi_duo@live.com>
2023-03-27 23:57:26 +0800

    feat: http proxy

--- src/service/service.go
+++ src/service/service.go
@@ -33,5 +33,30 @@ func (s *Service) handleConnection(conn net.Conn) {
 	defer conn.Close()
 	s.subCons[conn.RemoteAddr().String()] = conn
 	// remove conn from subCons if conn is closed
-	
+
+}
+
+// remove conn from subCons if conn is closed
+func (s *Service) removeConn(conn net.Conn) {
+	delete(s.subCons, conn.RemoteAddr().String())
+}
+
+// monitor conn
+func (s *Service) monitorConn(conn net.Conn) {
+	for {
+		// read from conn
+		// if error, remove conn from subCons
+		data, err := conn.Read()
+		if err != nil {
+			s.removeConn(conn)
+			break
+		}
+		// if read data, send data to all subCons
+		for _, subConn := range s.subCons {
+			_, err := subConn.Write(data)
+			if err != nil {
+				s.removeConn(subConn)
+			}
+		}
+	}
 }
