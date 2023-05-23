1eb42bc3e86b3928c7a84d583c721f747451ffbe
lpxxn <mi_duo@live.com>
2023-05-23 22:09:03 +0800

    feat: proxy http

--- src/service/httpredirect/httpredirectlistener_test.go
+++ src/service/httpredirect/httpredirectlistener_test.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"bytes"
 	"context"
+	"io"
 	"net"
 	"net/http"
 	"net/http/httptest"
@@ -11,6 +12,8 @@ import (
 	"net/url"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 type testConn struct {
@@ -67,15 +70,65 @@ func TestReverse(t *testing.T) {
 }
 
 func TestReverse2(t *testing.T) {
+
+	go func() {
+		go func() {
+			http.ListenAndServe(":5678", &TestHandler{t: t})
+		}()
+		http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("my-header", "my-header-value")
+			w.Write([]byte("hello2222"))
+		})
+		http.ListenAndServe(":5679", nil)
+	}()
+	time.Sleep(time.Second)
+	req, err := http.NewRequest(http.MethodGet, "http://127.0.0.1:5678/test", nil)
+	resp, err := http.DefaultClient.Do(req)
+	assert.Nil(t, err)
+	assert.NotNil(t, resp)
+
+}
+
+type TestHandler struct {
+	t *testing.T
+}
+
+func (h *TestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t := h.t
 	dialer := &net.Dialer{
 		Timeout: time.Duration(time.Second * 30),
 	}
+	conn, err := dialer.Dial("tcp", ":5679")
+	assert.Nil(t, err)
+	assert.NotNil(t, conn)
+
 	dialContext := func(ctx context.Context, network, address string) (net.Conn, error) {
-		return dialer.DialContext(ctx, network, address)
+		t.Logf("dialContext: %s, %s", network, address)
+		return dialer.DialContext(ctx, network, ":5679")
 	}
 	rt := &http.Transport{
 		Proxy:       http.ProxyFromEnvironment,
 		DialContext: dialContext,
 	}
 
+	newUrl, err := url.Parse("http://127.0.0.1:5679")
+	assert.Nil(t, err)
+	r.URL.Scheme = newUrl.Scheme
+	r.URL.Host = newUrl.Host
+	resp, err := rt.RoundTrip(r)
+	assert.Nil(t, err)
+	assert.NotNil(t, resp)
+
+	defer resp.Body.Close()
+
+	// Copy response headers
+	for k, v := range resp.Header {
+		//w.Header()[k] = v
+		w.Header().Set(k, v[0])
+	}
+
+	// Copy response status code
+	//err = resp.Write(w)
+	_, err = io.Copy(w, resp.Body)
+	assert.Nil(t, err)
 }
